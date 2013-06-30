da6df3d757592a83cda09a59780381f9bd0f2978
RyanCccc <chenrd769@gmail.com>
2013-06-29 21:41:13 -0400

    deploy

--- PCS/settings.py
+++ PCS/settings.py
@@ -24,8 +24,8 @@ DATABASES = {
 }
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts

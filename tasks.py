295aa4a3f962207164ec407c659d4a1215103a9f
RyanCccc <chenrd769@gmail.com>
2013-07-11 19:22:25 -0400

    change to english

--- tasks.py
+++ tasks.py
@@ -27,8 +27,9 @@ def update_periodic():
         sec.current_seats_num = curr_num
         sec.remain_seats_num = rem_num
         sec.save()
-        msg = '您订阅的课 %s ,课号 %s, Section Number是%s, CRN为%s, 一共有%d个位置, 现在还剩下%d' % (name, code, number, sec.crn, max_num, rem_num)
+        msg = 'Your subscribed class %s, class code is %s, class number is %s, section number is %s, maximun seats %s, there are %s seats left' % (name, code, number, sec.crn, max_num, rem_num)
         if seats_change > 0:
-            msg = 'Wow!!! 您的课号 %d 有新的位置啦！赶紧去抢下吧！！\n' % sec.crn + msg
+            msg = 'Wow! your class %s has new seats released!!\n' % sec.crn + msg
         elif seats_change < 0:
-            msg = 'Sorry!!! 您的课号 %d 的位置又在减少，要小心了哟。。\n' % sec.crn + msg
+            msg = 'Sorry!!! You class %s seats are decreasing!!\n' % sec.crn + msg
+        print msg

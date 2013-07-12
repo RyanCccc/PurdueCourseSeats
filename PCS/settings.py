726cba775eec7505be7e842d095e5346bb7ff293
RyanCccc <chenrd769@gmail.com>
2013-07-12 16:44:19 -0400

    change word asset to static

--- PCS/settings.py
+++ PCS/settings.py
@@ -78,18 +78,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path.join(PROJECT_DIR, 'assets'),
+    path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
